926d9fcab7f5ab69c74b7b4cb0a9cfec817304d6
songjein <jeinsong100@gmail.com>
2019-08-08 13:05:03 +0000

    lambda

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# image-resize-lambda
-image resize server &amp; aws lambda
+# image-resize-lambda 
+aws lambda용 이미지 리사이즈 서버 코드 (serverless)
+
+## 접근 주소 형식
+- http://API?url=이미지주소&width=너비&height=높이
+- 예시: https://abcde.execute-api.ap-northeast-2.amazonaws.com/latest?url=[s3이미지주소.jpeg]&width=100
+
+## IAM 유저 생성 후 ~/.aws/credentials 작성
+```
+[claudia]
+aws_access_key_id = '' 
+aws_secret_access_key = '' 
+```
+- ~/.bashrc에 export AWS_PROFILE=claudia 추가
---
+++ app.js
@@ -0,0 +1,61 @@
+'use strict'
+
+// sharp docs: https://github.com/lovell/sharp
+// sharp tutorial: https://malcoded.com/posts/nodejs-image-resize-express-sharp
+// sharp width request: https://github.com/lovell/sharp/issues/247
+
+const path = require('path');
+const sharp = require('sharp');
+const express = require('express');
+const request = require('request');
+const urlencode = require('urlencode');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const widthStr = req.query.width;
+  const heightStr = req.query.height;
+  let imgUrl = req.query.url;
+
+  let width, height;
+  if (widthStr) {
+    width = parseInt(widthStr);	
+  }
+  if (heightStr) {
+    height = parseInt(heightStr);	
+  }
+
+  const split = imgUrl.split('/');
+  const path = split.slice(0, split.length-1).join('/');
+  const filename = split.slice(-1)[0];
+
+  imgUrl = path + '/' + urlencode(filename);
+
+  let contentType = undefined;
+  const ext = filename.toLowerCase().split('.')[1]
+  if ('gif' == ext) {
+    contentType = 'gif'; 
+  } else if ('jpg' == ext || 'jpeg' == ext) {
+    contentType = 'jpeg';
+  } else if ('png' == ext) {
+    contentType = 'png';
+  } 
+  if (contentType === undefined) {
+    res.send('not supported image type');
+    return;
+  }
+  res.set('Content-Type', `image/${contentType}`);
+  
+  request({url: imgUrl, encoding: null}, function(error, response, buffer) {
+    const { statusCode, statusMessage } = response;
+    if (statusCode == 403 || statusCode == 404) {
+      res.send(statusMessage) 
+      return;
+    }
+    sharp(buffer)
+      .resize(width, height)
+      .pipe(res);
+  });
+});
+
+module.exports = app;
