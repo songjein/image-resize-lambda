1efbcdaf45c3450127d032374fa9ad702d3128fc
songjein <jeinsong100@gmail.com>
2019-08-10 14:40:40 +0000

    make cache file in s3

--- app.js
+++ app.js
@@ -4,33 +4,28 @@
 // sharp tutorial: https://malcoded.com/posts/nodejs-image-resize-express-sharp
 // sharp width request: https://github.com/lovell/sharp/issues/247
 
+const AWS = require('aws-sdk');
 const path = require('path');
 const sharp = require('sharp');
+const crypto = require('crypto');
 const express = require('express');
 const request = require('request');
 const urlencode = require('urlencode');
 
+const BUCKET = 'resizecache.feeeld';
+const REGION = 'ap-northeast-2';
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-  const widthStr = req.query.width;
-  const heightStr = req.query.height;
-  let imgUrl = req.query.url;
 
-  let width, height;
-  if (widthStr) {
-    width = parseInt(widthStr);	
-  }
-  if (heightStr) {
-    height = parseInt(heightStr);	
-  }
+AWS.config.update({
+  "region": REGION,
+});
+const s3 = new AWS.S3();
 
-  const split = imgUrl.split('/');
-  const path = split.slice(0, split.length-1).join('/');
-  const filename = split.slice(-1)[0];
 
-  imgUrl = path + '/' + urlencode(filename);
-
+const setContentType = (filename, res) => {
   let contentType = undefined;
   const ext = filename.toLowerCase().split('.').slice(-1)[0];
   if ('gif' == ext) {
@@ -45,17 +40,66 @@ app.get('/', (req, res) => {
     return;
   }
   res.set('Content-Type', `image/${contentType}`);
+}
+
+
+app.get('/', async (req, res) => {
+  const params = req.query; 
+  if (!params.url|| !params.size) {
+    res.status(500).send('invalid req format');
+    return;
+  }
+
+  const imageSize = params.size.toLowerCase().split("x");
+  let width = parseInt(imageSize[0]); 
+  let height = parseInt(imageSize[1]); 
+  width = width == 0 ? undefined : width;
+  height = height == 0 ? undefined : height;
   
-  request({url: imgUrl, encoding: null}, function(error, response, buffer) {
+  const imgUrl = params.url;
+  const urlSegList = imgUrl.split('/');
+  const path = urlSegList.slice(0, urlSegList.length-1).join('/');
+  const filename = urlSegList.slice(-1)[0];
+  setContentType(filename, res);
+
+  const encodedUrl = path + '/' + urlencode(filename); // inner communication or not? 
+  const originalKey = crypto.createHash('md5').update(encodedUrl).digest('hex') + '/' + imageSize;
+  
+  const getParams = {
+    Bucket: BUCKET,
+    Key: originalKey, 
+  }
+  // cache hit?
+  try {
+    await s3.headObject(getParams).promise(); // existence check
+    s3.getObject(getParams).createReadStream().pipe(res);
+    console.info(`cache hit! (${ originalKey } - ${ encodedUrl })`);
+    console.info('successfully served');
+    return;
+  } catch (err) {
+    console.info(`cache miss! (${ originalKey } - ${ encodedUrl })`, err);
+  }
+  
+  // cache miss!
+  request({url: encodedUrl, encoding: null}, async (error, response, buffer) => {
     const { statusCode, statusMessage } = response;
     if (statusCode == 403 || statusCode == 404) {
       res.send(statusMessage) 
       return;
     }
-    sharp(buffer)
-      .resize(width, height)
-      .pipe(res);
+    const stream = sharp(buffer).resize(width, height);
+    const uploadBuffer = await stream.toBuffer();
+    const uploadParams = { Bucket: BUCKET, Key: originalKey, Body: uploadBuffer };
+    try {
+      await s3.putObject(uploadParams).promise();
+      console.info(`Successfully uploaded data to ${ BUCKET }/${ originalKey }`);
+    } catch (e) {
+      console.error('S3 upload error', e);
+    }
+    stream.pipe(res);
+    console.info('successfully served');
   });
 });
+
 
 module.exports = app;
